82f39c19dc19ad48ebe9302ef16b8bc8196a8f8a
uicosp <uicosp@gmail.com>
2023-07-11 17:24:14 +0800

    fix spark_metrics.py

--- bin/spark_metrics.py
+++ bin/spark_metrics.py
@@ -5,20 +5,31 @@ import sys
 from functools import reduce
 
 
-def retry(url):
+def retry(url, fn=None):
     count = 10
     while count > 0:
         resp = requests.get(url)
         if resp.status_code == 404:
             time.sleep(3)
             count = count - 1
+        elif fn is not None:
+            data = resp.json()
+            if fn(data):
+                return data
+            else:
+                print("not ok")
+                print(data['attempts'][0]['completed'])
+                time.sleep(3)
+                count = count - 1
         else:
             return resp.json()
 
 
 def process(history_server, application_id):
-    stages = retry('{}/api/v1/applications/{}/1/stages'.format(history_server, application_id))
-    job = retry('{}/api/v1/applications/{}'.format(history_server, application_id))
+    job = retry('{}/api/v1/applications/{}'.format(history_server, application_id),
+                lambda data: data['attempts'][0]['completed'] is True)
+    attempt_id = job['attempts'][0]['attemptId']
+    stages = retry('{}/api/v1/applications/{}/{}/stages'.format(history_server, application_id, attempt_id))
     duration = round(job['attempts'][0]['duration'] / 1000, 2)
     fetch_times = round(reduce(lambda x, y: x + y, [stage['shuffleFetchWaitTime'] for stage in stages]) / 1000, 2)
     write_times = round(reduce(lambda x, y: x + y, [stage['shuffleWriteTime'] / 1e6 for stage in stages]) / 1000, 2)
