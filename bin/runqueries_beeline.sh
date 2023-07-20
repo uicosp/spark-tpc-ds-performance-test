672f5cee894054f4ca52922cdd7ddad28b7d41d9
uicosp <uicosp@gmail.com>
2023-07-20 14:53:48 +0800

    only use last rows selected

--- bin/runqueries_beeline.sh
+++ bin/runqueries_beeline.sh
@@ -31,7 +31,7 @@ do
   app_id=`echo $app_id_row | tr -s " " " " | cut -d " " -f3`
   echo "query${num} app_id=${app_id}"
   result=`python ${current_dir}/spark_metrics.py $SPARK_HISTORY_SERVER $RM_HTTP_ADDRESS $app_id`
-  lines=`cat ${OUTPUT_DIR}/query${num}.res | grep -E "row(s)? selected"`
+  lines=`cat ${OUTPUT_DIR}/query${num}.res | grep -E "row(s)? selected" | tail -n 1`
   echo "$lines" | while read -r line;
   do
     time=`echo $result | tr -s " " " " | cut -d " " -f1`
